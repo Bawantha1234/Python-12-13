cc0c09fc635b72c8bd8f4c51ac80ca0f80f6ef46
Bawantha1234 <bandarabawantha9@gmail.com>
2024-05-19 11:45:30 +0530

    string

--- index.py
+++ index.py
@@ -1,3 +1,6 @@
+import math
+
+
 print("hello world")
 
 
@@ -13,3 +16,26 @@ print(c)
 
 d = abs(20)
 print(d)
+
+
+v = math.sqrt(144)
+print(v)
+
+
+# Get Keyboard Input & Print terminal
+
+x = input("Enter name : ") # Pasindu
+print ("Hey" , x) # Output - Hey Pasindu
+input("Press<enter>")
+
+# String
+
+print ("he's","teacher")#Output - he's teacher
+input("Press<enter>")
+
+print('he\'s teacher') #back slash(\) using for in front of letter# // Output - he's teacher
+input("Press<enter>")
+
+print("bukey said \"hey there\" to me") #back slash (\) using for in front of letter# // Output - bukey said "hey there" to me
+input("Press<enter>")
+
